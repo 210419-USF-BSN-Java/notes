62b47e861d397b1e55adb8a289db6c1affd62fa5
Kevin Tran Huu <kevin.tranhuu@revature.com>
2021-04-23 15:07:54 -0500

    Adding today's notes

---
+++ week1/TDD-example/src/main/java/com/revature/exceptions/BoringNumberException.java
@@ -0,0 +1,31 @@
+package com.revature.exceptions;
+
+public class BoringNumberException extends Exception {
+
+	public BoringNumberException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BoringNumberException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BoringNumberException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BoringNumberException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BoringNumberException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ week1/TDD-example/src/main/java/com/revature/models/Calculator.java
@@ -0,0 +1,32 @@
+package com.revature.models;
+
+import com.revature.exceptions.BoringNumberException;
+import com.revature.interfaces.Calculatable;
+
+public class Calculator implements Calculatable{
+
+	@Override
+	public double add(double x, double y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double subtract(double x, double y) throws BoringNumberException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double multiply(double x, double y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double divide(double x, double y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
