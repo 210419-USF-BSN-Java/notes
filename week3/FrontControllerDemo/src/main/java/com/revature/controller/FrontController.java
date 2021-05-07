042492212290b05bee86b316a0c6c683f18f4483
Ryan-Choy <ryandchoy@gmail.com>
2021-05-07 11:37:12 -0700

    fixing

--- week3/FrontControllerDemo/src/main/java/com/revature/controller/FrontController.java
+++ week3/FrontControllerDemo/src/main/java/com/revature/controller/FrontController.java
@@ -8,38 +8,52 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.servlets.DefaultServlet;
 
+/*
+ * Using the FC Design pattern, all of the requests have been mapped to this servlet
+ */
 public class FrontController extends DefaultServlet {
 	
-	/*
-	 * All of the requests are redirected to this controller
-	 * 		- should handle requests for all endpoints and methods
+	/**
+	 * 
 	 */
+	private static final long serialVersionUID = 1L;
+
+	// RequestHelper(RH) is in charge of routing the request to their appropriate delegates based on the request URL
 	private RequestHelper rh = new RequestHelper();
 	
+	//All of the requests (endpoints, HTTP methods) are funneled through this method to be provided to the RH
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-		System.out.println("Request URI: " + request.getRequestURI());
-		System.out.println("Request ContextPath: " + request.getContextPath());
-		System.out.println("Request RequestURL: " + request.getRequestURL());
-		
-		// Stripping the URL to point to the appropriate delegate
+
+		/*
+		 * Initially the request URI might look something like: /FrontControllerDemo/planets/1
+		 * the URI is cleaned by returning a substring removing "/FrontcontrollerDemo" (the context path)
+		 */
 		String path = request.getRequestURI().substring(request.getContextPath().length());
-		System.out.println(path);
 		
 		/*
-		 * Based on the url, we want to map the request to the appropriate URL
+		 * We want to process the request based on the URI, however we still want to be able to return static webpages from the static folder
 		 */
-		if(path.startsWith("/static/")) {
+		if(path.startsWith("/static/") || path.equals("/") || path.equals("/index.html") ) {
 			super.doGet(request, response);
 		}else {
+			/*
+			 * Feeding the request and response object through the Request Handler logic
+			 */
 			rh.processRequest(request, response);
-			// Route the request to the appropriate delegate using the RequestHelper
 		}
 	}
 	
-	// We want to make sure that all of the HTTP methods are routed to the same logic
+	// We want to make sure that all of the HTTP methods are routed to the same method
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		doGet(request, response);
 	}
 	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		doGet(request, response);
+	}
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		doGet(request, response);
+	}
 	
 }
