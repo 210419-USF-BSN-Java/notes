751dd85d2591a4f3d150380abe0133c2edf75feb
Kevin Tran Huu <kevin.tranhuu@revature.com>
2021-05-07 13:30:07 -0500

    Adding Front Controller demo

--- week3/FrontControllerDemo/src/main/java/com/revature/delegates/PlanetDelegate.java
+++ week3/FrontControllerDemo/src/main/java/com/revature/delegates/PlanetDelegate.java
@@ -13,23 +13,65 @@ import com.revature.models.Planet;
 import com.revature.services.PlanetService;
 
 public class PlanetDelegate implements Delegatable {
-	
+
 	private PlanetService ps = new PlanetService();
-	
+	private ObjectMapper om = new ObjectMapper();
+
 	@Override
 	public void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		System.out.println(request.getMethod());
-		switch (request.getMethod()) {
+
+		/*
+		 * Path received has been stripped of [...]/planets
+		 * If path:
+		 * 		- {id}
+		 * 			- GET: Return planet of that id
+		 * 		- ""
+		 * 			- GET: Return all planets
+		 * 			- POST: Create a planet
+		 */
+		String path = (String) request.getAttribute("path");
+
+		if (path == null || path.equals("")) {
+			switch (request.getMethod()) {
 			case "GET":
 				List<Planet> planets = ps.getAllPlanets();
 				PrintWriter pw = response.getWriter();
-					pw.write(new ObjectMapper().writeValueAsString(planets));
+				pw.write(om.writeValueAsString(planets));
 				break;
 			case "POST":
-				// logic to add a planet
+				Planet p = new Planet();
+				p.setName(request.getParameter("name"));
+				p.setColor(request.getParameter("color"));
+				p.setId(ps.createPlanet(p));
+				response.setStatus(201);
+				System.out.println("Hey");
+				response.sendRedirect("planets");
+				
 				break;
 			default:
 				response.sendError(400, "Request not supported.");
+			}
+		} else {
+			int planetId = Integer.valueOf(path);
+			Planet p = null;
+
+			switch (request.getMethod()) {
+			case "GET":
+				p = ps.getPlanetById(planetId);
+				if (p != null) {
+					response.getWriter().write(om.writeValueAsString(p));
+				}else {
+					response.sendError(404, "Planet not found.");
+				}
+			case "PUT":
+				// logic to update a planet
+				break;
+			case "DELETE":
+				//logic to remove a planet
+				break;
+			default:
+				response.sendError(400, "Method not supported.");
+			}
 		}
 	}
 
