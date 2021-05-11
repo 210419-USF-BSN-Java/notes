4176893aa85bc513a1fa49e8ec037dc8ccd1fa90
Kemo Allen <kemo@claudia-allens-iMac-5.local>
2021-05-11 13:11:00 -0500

    Ajax work

--- week4/js2/ajax.js
+++ week4/js2/ajax.js
@@ -2,6 +2,8 @@ let apiURL = 'https://pokeapi.co/api/v2/pokemon/';
 
 document.getElementById('getData').onclick = getData;  // Assign functionality
 
+sb.onClick = getSprite;
+
 function getData(){
     
     // Retrieving the user input, in this case the pokemon id to be retrieved
@@ -44,4 +46,31 @@ function populateData(response){
         Use DOM Manipulation to write the data into our HTML page in the section tag.
     */
    console.log(response);
+
+   let section = document.getElementById('data');
+   section.innerHTML = 
+   `
+        <h1>Name: ${response.name}</h1>
+        <h1>Height: ${response.height}</h1>
+        <h1>Weight: ${response.weight}</h1>
+        <h1>ID: ${response.id}</h1>
+        <img src =${response.sprites.back_default}>
+        <img src =${response.sprites.front_default}>
+        
+   `
+
+   for(var i = 0; i < response.abilities.length; i++){
+    section.innerHTML += 
+    `
+        <h2>Ability ${i}: ${response.abilities[i].ability.name}</h2>
+    `
+   }
+   
+//    let i = 0;
+//    section.innerHTML += `<h3>Type: </h3>`
+//     do{
+//         section.innerHTML += `<h3>${response.types[i].type.name}</h3>`
+//         i++;
+//     }while (response.types[i] != null)
+
 }
